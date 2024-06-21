f86012e714ee6d8b67f23d640264b9f58c65a2d0
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-06-21 09:22:35 +0530

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,11 @@
+public class MathDemo
+{
+public static void main(String args[])
+{
+double x=9;
+double y=25;
+System.out.println("Math.sqrt("+x+")="+Math.sqrt(x));
+System.out.println("Math.sqrt("+y+")="+Math.sqrt(y));
+}
+}
+
