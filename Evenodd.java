def119c0a7265a7956da0432dbb096032341c523
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-06-21 09:21:40 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Evenodd
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
+}
+}
