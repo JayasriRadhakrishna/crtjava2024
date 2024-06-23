347b3238e48b3df86ce0cbef84e4b2af766f8fcb
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-06-23 22:04:11 +0530

    Add files via upload

---
+++ Prime1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Prime1
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int c=0,r=0;
+if(n==1)
+{
+c=1;
+}
+for(int i=2;i<=n/2;i++)
+{
+r++;
+if(n%i==0)
+{
+c++;
+break;
+}
+}
+System.out.println(r+" "+c);
+if(c==0)
+System.out.println("prime no");
+else
+System.out.println("not a prime no");
+}
+}
+
