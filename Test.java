de39a6c185ffc69f00bebf3833e4394a5d39e97f
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-06-23 22:02:11 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Test
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int tot=0;
+for(int i=0;i<=n;i++)
+{
+tot=tot+i;
+}
+System.out.println(tot);
+}
+}
+
