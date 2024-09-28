e2415d1162ce2b3d84659f52f673ebbfcb03150f
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-09-28 21:10:04 +0530

    Add files via upload

---
+++ Aloop.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Aloop 
+{
+ public static void main(String[] args) 
+ {
+ ArrayList<String> list = new ArrayList<>();
+ list.add("Apple");
+ list.add("Banana");
+ list.add("Cherry");
+for (int i = 0; i < list.size(); i++) 
+{
+ System.out.println(list.get(i));
+}
+}
+}
