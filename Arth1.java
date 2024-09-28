c42823d5eef7944df9565da017763cbcc324e227
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-09-28 21:12:57 +0530

    Add files via upload

---
+++ Arth1.java
@@ -0,0 +1,22 @@
+package jai;
+public class Arth1
+{
+public void add(int x,int y)
+{
+System.out.println(x+y);
+}
+public void sub(int x,int y)
+{
+System.out.println(x-y);
+}
+public void mul(int x,int y)
+{
+System.out.println(x*y);
+}
+public void div(int x,int y)
+{
+System.out.println(x/y);
+}
+}
+
+
