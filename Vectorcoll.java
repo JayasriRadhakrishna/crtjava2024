c1705ce87823cad610abafd3764c850ecb7ef999
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-09-28 21:07:09 +0530

    Add files via upload

---
+++ Vectorcoll.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Vectorcoll
+{
+public static void main(String args[])
+{
+Vector<Integer> v1=new Vector<>();
+System.out.println(v1.capacity());
+for(int i=1;i<=20;i++)
+v1.add(i);
+System.out.println(v1);
+System.out.println(v1.capacity());
+v1.add(88);
+System.out.println(v1);
+System.out.println(v1.capacity());
+System.out.println(v1.size());
+}
+}
