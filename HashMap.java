9e5020ed76155aba636775fb973dd85fc6d8b938
JayasriRadhakrishna <152085973+JayasriRadhakrishna@users.noreply.github.com>
2024-09-28 21:21:20 +0530

    Add files via upload

---
+++ HashMap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class HashMap
+{
+public static void main(String args[])
+{
+HashMap h1=new HashMap();
+h1.put(101,"sss");
+h1.put(123,"rrr");
+h1.put(105,"kkk");
+System.out.println(h1);
+h1.put(333,666);
+h1.put("aaa","ddd");
+h1.put("maths",88);
+system.out.println(h1);
+h1.put(101,"bbb");
+System.out.println(h1);
+}
+}
